2f7f789f2e2a2dc837dcf82301a76e29d8d0f9a6
Die <growder1@gmail.com>
2024-09-28 01:38:29 +0500

    Init

---
+++ src/main/java/com/example/naumenjavatask2/NaumenJavaTask2Application.java
@@ -0,0 +1,13 @@
+package com.example.naumenjavatask2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaumenJavaTask2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaumenJavaTask2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/naumenjavatask2/NaumenJavaTask2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.naumenjavatask2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaumenJavaTask2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
